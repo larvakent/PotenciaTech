dc085bf5b716f3a9e9f9f5e32fe5debb5e6c030f
Lavinia <lavinia.santos2105@gmail.com>
2022-11-24 12:30:41 -0300

    identação

---
+++ ConhecendoJava/SintaxeJava/anatomia-classes/src/BoletimEstudantil.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class BoletimEstudantil {
+
+    public static void main(String[] args) {
+        
+        int mediaFinal;
+        
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Insira a média do aluno: ");
+        mediaFinal = leitor.nextInt();
+
+        if(mediaFinal < 6){
+            System.out.println("REPROVADO");
+        } else {
+            if(mediaFinal == 6) {
+                System.out.println("RECUPERAÇÃO");
+            } else {
+                System.out.println("APROVADO");
+            }
+        }
+    }
+    
+}
