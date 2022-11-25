6e70b76a29d88b80e0c1da120ebc227887071130
Lavinia <lavinia.santos2105@gmail.com>
2022-11-25 08:39:36 -0300

    tipos e variaveis

---
+++ ConhecendoJava/SintaxeJava/tipos-variaveis/src/edu/tipos/tiposVariaveis.java
@@ -0,0 +1,15 @@
+package edu.tipos;
+
+public class tiposVariaveis {
+
+    public static void main(String[] args) {
+        
+        byte idade = 123;
+        short ano = 2022;
+        int cep = 58077110; //se começar com 0 talvez tenha que usar um outro tipo
+        long cpf = 10938746723L; //se começar com 0 talvez tenha que usar um outro tipo
+        float pi = 3.14f;
+        double salario = 1233.45d;
+    }
+    
+}
