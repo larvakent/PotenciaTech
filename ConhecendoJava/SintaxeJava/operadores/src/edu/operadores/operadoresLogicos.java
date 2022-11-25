a838dff90e6d64ba3d1561662d3e61e707ade08c
Lavinia <lavinia.santos2105@gmail.com>
2022-11-25 09:32:13 -0300

    relacionais com objetos

--- ConhecendoJava/SintaxeJava/operadores/src/edu/operadores/operadoresLogicos.java
+++ ConhecendoJava/SintaxeJava/operadores/src/edu/operadores/operadoresLogicos.java
@@ -3,27 +3,10 @@ package edu.operadores;
 public class operadoresLogicos {
 
     public static void main(String[] args) {
-        int a, b;
+        String a = "Vania";
+        String b = new String("Vania");
         
-        a = 1;
-        b = 2;
-
-        boolean res = a==b;
-
-        if(a <b){
-            System.out.println("A nossa condição é verdadeira.");
-        }
-        
-        System.out.println("O número um é igual ao número dois? " + res);
-        
-        res = a != b;
-
-        System.out.println("O número um é diferente do número dois? " + res);
-
-        res = a > b;
-
-        System.out.println("O número um é maior que o número dois? " + res);
-          
+        System.out.println(a.equals(b));
     }
     
 }
