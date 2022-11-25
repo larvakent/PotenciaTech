cc235dbd71c0d7e2ed001e5b7d6364bfbbeeea00
Lavinia <lavinia.santos2105@gmail.com>
2022-11-25 09:19:26 -0300

    ternario

---
+++ ConhecendoJava/SintaxeJava/operadores/src/edu/operadores/operadoresLogicos.java
@@ -0,0 +1,25 @@
+package edu.operadores;
+
+public class operadoresLogicos {
+
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        String res = a==b ? "verdadeiro" : "falso";
+
+        System.out.println(res);
+
+
+        /*if(a==b){
+            res = "verdadeiro";
+        } else {
+            res = "falso";
+        }
+
+        System.out.println(res);*/       
+    }
+    
+}
