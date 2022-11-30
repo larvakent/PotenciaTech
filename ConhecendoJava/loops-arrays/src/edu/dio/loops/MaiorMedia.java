ca889094d72e07f91c5056415997763ce496b607
Lavinia <lavinia.santos2105@gmail.com>
2022-11-30 15:35:09 -0300

    exercicio 3

--- ConhecendoJava/loops-arrays/src/edu/dio/loops/MaiorMedia.java
+++ ConhecendoJava/loops-arrays/src/edu/dio/loops/MaiorMedia.java
@@ -7,11 +7,21 @@ public class MaiorMedia {
 
         int numero;
         int cont = 0;
+        int maior = 0;
+        int soma = 0;
         
         do {
            System.out.println("Número: ");
            numero = leia.nextInt();
+           soma = soma + numero;
+
+            if(numero > maior) maior =  numero;
+
            cont = cont + 1;
         } while(cont < 5);
+
+        System.out.println("Maior número: " + maior);
+        System.out.println("Soma dos números: " + soma);
+        System.out.println("Média dos números: " + (soma/5));
     }
 }
