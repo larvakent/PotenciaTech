0520c5ee8a883b6e5e1cd7aa71bd8ed19e66f9b3
Lavinia <lavinia.santos2105@gmail.com>
2022-11-30 19:24:08 -0300

    exercicio 4

---
+++ ConhecendoJava/loops-arrays/src/edu/dio/loops/ParImpar.java
@@ -0,0 +1,34 @@
+package edu.dio.loops;
+
+import java.util.Scanner;
+
+public class ParImpar {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        int qntdNum;
+        int cont = 0;
+        int num;
+        int qntdPar = 0;
+        int qntdImpar = 0;
+
+        System.out.println("Quantidade de números: ");
+        qntdNum = leia.nextInt();
+
+        do {
+            System.out.println("Número: ");
+            num = leia.nextInt();
+            if(num%2==0){
+                qntdPar++;
+            } else {
+                qntdImpar++;
+            }
+            cont++;
+        } while(cont < qntdNum);
+
+        System.out.println("Quantidade de pares: " + qntdPar);
+        System.out.println("Quantidade de impares: " + qntdImpar);
+
+
+
+    }
+}
